11a43915e8eff65ef2457a1f52f870532bc2b258
jakefair <100549835+jakefair97@users.noreply.github.com>
2023-07-21 22:36:53 -0500

    updated utils file in client to follow gql

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,45 @@
+import { gql } from '@apollo/client';
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      profile {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($userId: ID!, $book: String!) {
+    saveBook(userId: $userId, book: $book) {
+      _id
+      name
+      books
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      profile {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($book: String!) {
+    removeBook(book: $book) {
+      _id
+      name
+      books
+    }
+  }
+`;

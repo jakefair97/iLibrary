16ee6366360835bf6e01b5003acd5cdbe7dd8d4f
jakefair <100549835+jakefair97@users.noreply.github.com>
2023-07-21 23:27:08 -0500

    finished adjustments to resolvers and typedefs in server

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,15 +4,8 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    users: async () => {
-      return User.find();
-    },
-
-    user: async (parent, { userId }) => {
-      return User.findOne({ _id: userId });
-    },
     // By adding context to our query, we can retrieve the logged in user without specifically searching for them
-    me: async (parent, args, context) => {
+    me: async (parent, context) => {
       if (context.user) {
         return User.findOne({ _id: context.user._id });
       }
@@ -60,13 +53,6 @@ const resolvers = {
         );
       }
       // If user attempts to execute this mutation and isn't logged in, throw an error
-      throw new AuthenticationError('You need to be logged in!');
-    },
-    // Set up mutation so a logged in user can only remove their profile and no one else's
-    removeUser: async (parent, args, context) => {
-      if (context.user) {
-        return User.findOneAndDelete({ _id: context.user._id });
-      }
       throw new AuthenticationError('You need to be logged in!');
     },
     // Make it so a logged in user can only remove a skill from their own profile
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -34,14 +34,12 @@ type User {
   }
 
   type Query {
-    # Because we have the context functionality in place to check a JWT and decode its data, we can use a query that will always find and return the logged in user's data
     me: User
   }
 
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-
     saveBook(book: SavedBookInput): User
     removeBook(bookId: String!): User
   }
